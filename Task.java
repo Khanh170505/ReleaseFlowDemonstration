56181cdfc988cbfffb34bac77d5765aef861421a
NguyenHan1910 <nguyenxuanngochan1910@gmail.com>
2025-09-30 02:38:31 +0700

    Task.java

--- Task.java
+++ Task.java
@@ -1,11 +1,17 @@
 public class Task {
     private String description;
+    private int priority; 
 
-    public Task(String description) {
+    public Task(String description, int priority) {
         this.description = description;
+        this.priority = priority;
     }
 
     public String getDescription() {
         return description;
+    }
+
+    public int getPriority() {
+        return priority;
     }
 }
